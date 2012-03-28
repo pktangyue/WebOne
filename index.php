a9c6b4ce61b879cabd3bcfb4aece90277c05e95a
tangyue <tangyue1004@gmail.com>
2012-03-29 01:08:50 +0800

    zhonghewenyi neirong&wenzi

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 			<p><a href="/zhonghewenyi/index.php">03综合文艺</a></p>
 			<p><a href="zhonghewenyi/liebiao.php">04综合文艺—列表</a></p>
 			<p><a href="zhonghewenyi/tuwen.php">05综合文艺—图文列表</a></p>
+			<p><a href="zhonghewenyi/neirong.php">06综合文艺—内容</a></p>
+			<p><a href="zhonghewenyi/wenzi.php">06综合文艺—文字内容</a></p>
 		</fieldset>
 	</body>
 </html>
---
+++ zhonghewenyi/neirong.php
@@ -0,0 +1,24 @@
+<? include '../common/header.php';?>
+<? include 'leftside.php';?>
+<div class="rightside">
+	<h1 class="rightside_title">【综合文艺】</h1>
+	<div class="rightside_content">
+		<div class="section">
+			<h3 class="section_title">季度影赛</h3>
+			<p class="list_nav"><a href="#">综合文艺</a><span class="c_green ml10 mr10">&gt;</span><a href="#">季度影赛</a><span class="c_green ml10 mr10">&gt;</span><span>铁路修到我家乡</span></p>
+			<div class="article">
+				<h2 class="article_title">铁路修到我家乡</h2>
+				<h5 class="article_subtitle"><span class="mr15">来源：人民铁道网-人民铁道报</span><span class="mr15">作者：谢影</span><span>发表时间:2010-11-19 14:21</span></h5>
+				<img class="noborder" width="510" height="350" src="/img/blank.jpg"/>
+				<p>青藏铁路开通运营，给青藏两省区的人民群众带来了致富的契机，架起了雪域高原与内地沟通的桥梁。我一直都在寻找这方面的摄影题材。摄影人对晨曦和晚霞都有偏爱，10月的一天清晨，我正在当雄县采访，浩瀚的羌塘草原上，牛羊如同散落的珍珠，一个牧羊姑娘眼睛直视前方，鲜艳的头巾同四周的环境形成鲜明的对比。此时，晨光斜照在远处念青唐古拉山上，蔚为壮观。这正好构成一幅青藏铁路旁优美的牧羊图。于是，我顾不上寒冷和缺氧，急忙来到羊群旁，记录下了这美妙的瞬间。当时我用的是尼康D200相机。为了加大景深，在光线暗的情况下我运用F11的光圈，1/80秒速度。</p>
+				<div class="tl mt20"><a class="support_btn" href="#">支持</a><span class="f14 ml20 c_999">已被支持：35</span></div>
+			</div>
+		</div>
+		<div class="clearfix">
+			<div class="article_prev">上一篇：<a href="#">来自记忆的花之册</a></div>
+			<div class="article_next">下一篇：<a href="#">梁小曼作品：收获</a></div>
+		</div>
+	</div>
+	<div class="rightside_bottom"></div>
+</div>
+<? include '../common/footer.php';?>
