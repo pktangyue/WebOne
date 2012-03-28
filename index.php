163847af4be6154abd255fca6252ae8300ff8bb6
tangyue <tangyue1004@gmail.com>
2012-03-29 00:24:45 +0800

    zhonghewenyi tuwen

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<fieldset>
 			<p><a href="/zhonghewenyi/index.php">03综合文艺</a></p>
 			<p><a href="zhonghewenyi/liebiao.php">04综合文艺—列表</a></p>
+			<p><a href="zhonghewenyi/tuwen.php">05综合文艺—图文列表</a></p>
 		</fieldset>
 	</body>
 </html>
---
+++ zhonghewenyi/tuwen.php
@@ -0,0 +1,68 @@
+<? include '../common/header.php';?>
+<? include 'leftside.php';?>
+<div class="rightside">
+	<h1 class="rightside_title">【综合文艺】</h1>
+	<div class="rightside_content">
+		<div class="section">
+			<h3 class="section_title">季度影赛</h3>
+			<ul class="imgs clearfix">
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+				<li>
+					<a href="#"><img src="/img/blank.jpg" width="165" height="165"/></a>
+					<p><a href="#">谍影虫从（103）</a></p>
+					<p>作者：皇家粮仓</p>
+				</li>
+			</ul>
+			<div class="pager">
+				<a href="#">首页</a>
+				<a href="#">上一页</a>
+				<span>1</span>
+				<a href="#">2</a>
+				<a href="#">3</a>
+				<a href="#">4</a>
+				<a href="#">5</a>
+				<a href="#">6</a>
+				<a href="#">7</a>
+				<a href="#">8</a>
+				<a href="#">下一页</a>
+				<a href="#">尾页</a>
+			</div>
+		</div>
+	</div>
+	<div class="rightside_bottom"></div>
+</div>
+<? include '../common/footer.php';?>
